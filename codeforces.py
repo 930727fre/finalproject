0fe373e9deb80c157d24b46b0a95736ce0cea885
Freddy Chuang <930727fre@gmail.com>
2023-04-03 22:08:45 +0800

    GET from codeforces api remove tqdm module

---
+++ codeforces.py
@@ -0,0 +1,16 @@
+import lxml
+import requests
+import bs4
+import time
+
+url="https://codeforces.com/api/contest.list"
+response=requests.get(url)
+requests_try=3
+while requests_try and response.status_code!=200:
+    requests_try -= 1
+    time.sleep(2)
+if response.status_code!=200:
+    print("unable to GET from ", url)
+    print("status code :", response.status_code)
+    print("reaon :", response.reason)
+    quit()
---
+++ codeforces_api_by_ChatGPT.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from datetime import datetime, timedelta
+
+# Set the API endpoint URL
+url = 'https://codeforces.com/api/contest.list'
+
+# Set the current date and the date three days from now
+now = datetime.now()
+end = now + timedelta(days=30)
+
+# Send a GET request to the API endpoint
+response = requests.get(url)
+
+# Parse the response JSON data
+data = json.loads(response.content)
+
+# Filter the contests that start within the next three days
+contests = []
+for contest in data['result']:
+    start_time = datetime.fromtimestamp(contest['startTimeSeconds'])
+    if start_time >= now and start_time <= end:
+        contests.append({
+            'name': contest['name'],
+            'start_time': start_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'duration': contest['durationSeconds'] // 3600
+        })
+
+# Save the contests data in a JSON file
+with open('codeforce.json', 'w') as f:
+    json.dump(contests, f)
