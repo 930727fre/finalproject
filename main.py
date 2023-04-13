2ff3099459399c3750749f4e46d8384bfd383c11
sansann2 <84257894+sansann2@users.noreply.github.com>
2023-04-13 16:04:02 +0800

    hide codeforces_api_by_chatGPT.py in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import lxml, os, requests, bs4
 import codeforces, codeforces_api_by_ChatGPT
 
 print("main.py:")
-print("codeforces_api_by_ChatGPT.py:")
-codeforces_api_by_ChatGPT.cf_ChatGPT()
+#print("codeforces_api_by_ChatGPT.py:")
+#codeforces_api_by_ChatGPT.cf_ChatGPT()
 print("codeforces.py:")
 codeforces.cf()
